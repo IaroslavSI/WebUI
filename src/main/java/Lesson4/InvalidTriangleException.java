f4e99328388c7c529bb5843fa026f7a6bbe43f22
IaroslavS <iaroslavs@yandex.ru>
2023-06-08 00:26:18 +0500

    Домашнее задание 4

---
+++ src/main/java/Lesson4/InvalidTriangleException.java
@@ -0,0 +1,9 @@
+package Lesson4;
+
+import java.util.*;
+
+public class InvalidTriangleException extends Exception{
+    public InvalidTriangleException() {
+        super("Треугольник не существует");
+    }
+}
